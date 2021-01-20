5bb83bece61c96004c8ad6122a191f0324170783
Pratyush <itispriyanshu@gmail.com>
2021-01-20 19:14:57 +0530

    Create translate.py

---
+++ deegram/utils/translate.py
@@ -0,0 +1,48 @@
+CREATOR = "@priyanshu_bhardwaj"
+BOT_NAME = "Song Dl Bot"
+WELCOME_MSG = (
+	f"🎶🎧 Welcome to {BOT_NAME} 🎶🎧\n\n"
+	"Click /help for more information."
+)
+INFO_MSG = (
+	f"Creator : {CREATOR}  🇮🇳\n"
+	f"Say Thanks if you using it :)"
+)
+STATS_MSG = (
+	"Bot Uptime: {}\n"
+	"Total disk space: {}\n"
+	"Used: {}\n"
+	"Free: {}"
+)
+HELP_MSG = (
+	"Search by album or tack or just send me a Deezer track or album link and I will download it for you :)\n\n"
+	"**List of all commands:**\n"
+	"/start - Get the welcome message\n"
+	"/help - Get this message\n"
+	"/settings - Change your preferences\n"
+	"/info - Get some useful information about the bot\n"
+	"/stats - Get some statistics about the bot\n"
+        "/source - source of this project ⭐\n"
+)
+SOURCE_MSG = (
+f"Creator : {CREATOR}  🇮🇳\n\n"
+	f"https://github.com/bhardwajjEE/TgSongDl"
+)
+DOWNLOAD_MSG = "Downloading…"
+UPLOAD_MSG = "Uploading…"
+END_MSG = "Finished."
+ALBUM_MSG = (
+	"💽 Album: {}\n"
+	"👤 Artist: {}\n"
+	"📅 Date: {}\n"
+	"🎧 Total tracks: {}"
+)
+TRACK_MSG = (
+	"🎧 Track: {}\n"
+	"👤 Artist: {}\n"
+	"💽 Album: {}\n"
+	"📅 Date: {}"
+)
+CHOOSE = "Choose:"
+SEARCH_ALBUM = "Search album 💽"
+SEARCH_TRACK = "Search track 🎧"
